b1d2b2f07cfa43ae2b19ccf80b76cffec42846ce
debugtheworldbot <istiancz@gmail.com>
2020-06-29 22:58:23 +0800

    init

---
+++ index.ts
@@ -0,0 +1,11 @@
+import * as http from "http";
+import {IncomingMessage, ServerResponse} from "http";
+
+const server = http.createServer();
+server.on('request', (request:IncomingMessage, response:ServerResponse) => {
+    console.log(request.method)
+    console.log(request.url)
+    console.log(request.headers)
+    response.end('ended')
+})
+server.listen('8888')
