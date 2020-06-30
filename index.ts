c3c7a5c0fcb7b62143efee540fafca7b2f433b4f
debugtheworldbot <istiancz@gmail.com>
2020-06-30 16:02:57 +0800

    return file based on url

--- index.ts
+++ index.ts
@@ -1,14 +1,33 @@
 import * as http from "http";
 import {IncomingMessage, ServerResponse} from "http";
+import * as fs from "fs";
+import * as p from 'path'
 
 const server = http.createServer();
-server.on('request', (request:IncomingMessage, response:ServerResponse) => {
-    const array=[]
-    request.on('data',(chunk)=>{
-        array.push(chunk)
-        const body=Buffer.concat(array).toString()
-        console.log(body)
-        response.end('ended')
-    })
+const publicDir = p.resolve(__dirname, 'public')
+server.on('request', (request: IncomingMessage, response: ServerResponse) => {
+    const {method, url, headers} = request
+    switch (url) {
+        case '/index.html':
+            fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
+                if (error) throw error
+                response.end(data.toString())
+            })
+            break
+        case '/style.css':
+            response.setHeader('Content-Type','text/css;charset=utf-8')
+            fs.readFile(p.resolve(publicDir, 'style.css'), (error, data) => {
+                if (error) throw error
+                response.end(data.toString())
+            })
+            break
+        case '/main.js':
+            response.setHeader('Content-Type','text/javascript;charset=utf-8')
+            fs.readFile(p.resolve(publicDir, 'main.js'), (error, data) => {
+                if (error) throw error
+                response.end(data.toString())
+            })
+            break
+    }
 })
 server.listen('8888')
---
+++ public/main.js
@@ -0,0 +1,6 @@
+console.log('hi')
+const h1=document.querySelector('h1')
+console.log(h1)
+h1.onclick=()=>{
+    alert('hi')
+}
