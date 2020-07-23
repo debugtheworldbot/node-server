6858181693efffd682d0dfd3d182aa99285f8c72
debugtheworldbot <istiancz@gmail.com>
2020-07-23 22:48:06 +0800

    return status code

--- index.ts
+++ index.ts
@@ -2,32 +2,33 @@ import * as http from "http";
 import {IncomingMessage, ServerResponse} from "http";
 import * as fs from "fs";
 import * as p from 'path'
-
+import * as url from "url";
+// ts-node-dev 监听
 const server = http.createServer();
-const publicDir = p.resolve(__dirname, 'public')
+const publicDir = p.resolve(__dirname, 'public') //public绝对路径
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-    const {method, url, headers} = request
-    switch (url) {
-        case '/index.html':
-            fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
-                if (error) throw error
-                response.end(data.toString())
-            })
-            break
-        case '/style.css':
-            response.setHeader('Content-Type','text/css;charset=utf-8')
-            fs.readFile(p.resolve(publicDir, 'style.css'), (error, data) => {
-                if (error) throw error
-                response.end(data.toString())
-            })
-            break
-        case '/main.js':
-            response.setHeader('Content-Type','text/javascript;charset=utf-8')
-            fs.readFile(p.resolve(publicDir, 'main.js'), (error, data) => {
-                if (error) throw error
-                response.end(data.toString())
-            })
-            break
+    const {method, url:path, headers} = request
+    const {pathname,search}=url.parse(path) // 查询参数
+    let filename=pathname.slice(1)
+    if(filename===''){
+        filename='index.html'
     }
-})
+            fs.readFile(p.resolve(publicDir, filename), (error, data) => {
+                if (error && error.errno===-4058) {
+                    response.statusCode=404
+                    response.end()
+                }else if(error.errno===-4068) {
+                    response.statusCode = 403
+                    response.end('cant get content ')
+                }
+                else if(error){
+                    response.statusCode=500
+                    response.end('server error')
+                }else {
+                    response.end(data.toString())
+                }
+            })
+
+    }
+)
 server.listen('8888')
