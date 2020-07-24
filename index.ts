7d0be0934ec860a2a247069b3faa6228cc4a30a4
debugtheworldbot <istiancz@gmail.com>
2020-07-24 08:30:20 +0800

    static server

--- index.ts
+++ index.ts
@@ -8,6 +8,11 @@ const server = http.createServer();
 const publicDir = p.resolve(__dirname, 'public') //public绝对路径
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
     const {method, url:path, headers} = request
+    if(method!=='GET'){
+        response.statusCode=405
+        response.end()
+        return
+    }
     const {pathname,search}=url.parse(path) // 查询参数
     let filename=pathname.slice(1)
     if(filename===''){
@@ -16,16 +21,19 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
             fs.readFile(p.resolve(publicDir, filename), (error, data) => {
                 if (error && error.errno===-4058) {
                     response.statusCode=404
-                    response.end()
-                }else if(error.errno===-4068) {
-                    response.statusCode = 403
-                    response.end('cant get content ')
+                    response.end('404')
                 }
+                //     else if(error.errno===-4068) {
+                //     response.statusCode = 403
+                //     response.end('cant get content ')
+                // }
                 else if(error){
                     response.statusCode=500
                     response.end('server error')
                 }else {
-                    response.end(data.toString())
+                    // 返回文件内容
+                    response.setHeader('Cache-Control','public,max-age=31536000') // 设置缓存时间为一年
+                    response.end(data)
                 }
             })
 
